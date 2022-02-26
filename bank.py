12d73d7f26f604045c369c26f49d748d1e13e351
abdullah_awad <abdullah.alashafi@gmail.com>
2022-02-26 13:31:25 +0300

    store and print user account recode

--- bank.py
+++ bank.py
@@ -4,13 +4,19 @@ class Bank:
         self.name = name
         self.password = password
         self.balance = 0
-    
+        self.recodes = []
+
     def get_balance(self):
-        return f'Your balance: {self.balance} SA {datetime.now()}'
+        return f'Your balance: {self.balance} SA'
+    
+    def get_recode(self):
+        for k, v in self.recodes:
+            print(f'{k} -- {v}')
     
     def deposit(self, amount):
         if isinstance(amount, int):
             self.balance += amount
+            self.recodes.append({f'Deposit {amount}': f'{amount} has been deposited to your account on {datetime.now()}'})
         else:
             return 'Must be number'
     
@@ -18,6 +24,7 @@ class Bank:
         if isinstance(amount, int):
             if self.balance >= amount:
                 self.balance -= amount
+                self.recodes.append({f'Draw {amount}': f'{amount} has been drawee from your account on {datetime.now()}'})
             else:
                 return 'Sorry you do not have balance'
         else:
